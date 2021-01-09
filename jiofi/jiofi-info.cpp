2137effaebf68c2cd1421a5455d3fa827f0e794e
v-i-s-h <get_vichu@yahoo.com>
2021-01-09 22:03:43 +0530

    added lan, wan, device and system info

--- jiofi/jiofi-info.cpp
+++ jiofi/jiofi-info.cpp
@@ -11,11 +11,42 @@ int main(int argc, char *argv[]) {
 
     JioFi m2;
 
-    result_map r = m2.lte_status();
-    for(auto k: r) {
+    auto r_lte = m2.lte_status();
+    std::cout << "=== LTE Status ===\n";
+    for(auto k: r_lte) {
         std::cout << k.first << "    "
-            << k.second << std::endl;
+            << k.second << "\n";
     }
+    std::cout << "------------------" << std::endl;
+
+    auto r_lan = m2.lan_info();
+    std::cout << "=== LAN Info ===\n";
+    for(auto k: r_lan) {
+        std::cout << k.first << "    " << k.second << "\n";
+    }
+    std::cout << "----------------" << std::endl;
+
+    auto r_wan = m2.wan_info();
+    std::cout << "=== WAN Info ===\n";
+    for(auto k: r_wan) {
+        std::cout << k.first << "    " << k.second << "\n";
+    }
+    std::cout << "----------------" << std::endl;
+
+    auto r_dev = m2.dev_details();
+    std::cout << "=== Device Details ===\n";
+    for(auto k: r_dev) {
+        std::cout << k.first << "    " << k.second << "\n";
+    }
+    std::cout << "----------------------" << std::endl;
+
+    auto r_sys = m2.sys_perf();
+    std::cout << "=== System Performance ===\n";
+    for(auto k: r_sys) {
+        std::cout << k.first << "    " << k.second << "\n";
+    }
+    std::cout << "--------------------------" << std::endl;
+
 
     return 0;
 }
--- jiofi/jiofi.hpp
+++ jiofi/jiofi.hpp
@@ -45,18 +45,17 @@ class JioFi {
                 deviceurl(url) {}
 
         result_map lte_status();
-        std::string lan_info();
-        std::string wan_info();
-        std::string dev_details();
-        std::string sys_perf();
-        std::string generic_info();
+        result_map lan_info();
+        result_map wan_info();
+        result_map dev_details();
+        result_map sys_perf();
 };
 
 result_map JioFi::lte_status() {
     const std::string cmd_url = "st_lte.w.xml";
     const std::string url = deviceurl + cmd_url;
 
-    // return info_json;
+    // fetch data in XML form
     std::string info_xml = fetch_data(url);
 
     result_map r;
@@ -142,58 +141,254 @@ result_map JioFi::lte_status() {
 
 }
 
-/*
-std::string JioFi::lan_info() {
+result_map JioFi::lan_info() {
     const std::string cmd_url = "st_lan.w.xml";
     const std::string url = deviceurl + cmd_url;
 
     std::string info_xml = fetch_data(url);
-    std::string info_json = xml2json(info_xml.c_str());
 
-    return info_json;
+    result_map r;
+
+    rapidxml::xml_document<> xml_data;
+    xml_data.parse<0>(&info_xml[0]);
+
+    rapidxml::xml_node<> *rootnode = xml_data.first_node();
+    if(rootnode) {
+        /*
+         * Extract fields
+         */
+        rapidxml::xml_node<> *node;
+
+        // SSID Info
+        node = rootnode->first_node("ssid");
+        if(node) {
+            r["ssid"] = node->value();
+        }
+
+        // DHCP Status
+        node = rootnode->first_node("dhcp");
+        if(node) {
+            r["dhcp"] = node->value();
+        }
+
+        // Subnetmask
+        node = rootnode->first_node("submask");
+        if(node) {
+            r["submask"] = node->value();
+        }
+
+        // Gateway
+        node = rootnode->first_node("gate");
+        if(node) {
+            r["gate"] = node->value();
+        }
+
+        // MAC Address
+        node = rootnode->first_node("macaddr");
+        if(node) {
+            r["macaddr"] = node->value();
+        }
+
+        // User count
+        node = rootnode->first_node("user_cnt");
+        if(node) {
+            r["user_cnt"] = node->value();
+        }
+
+        // Channel Selction
+        node = rootnode->first_node("ch");
+        if(node) {
+            r["ch"] = node->value();
+        }
+
+        // Channel ID
+        node = rootnode->first_node("channel");
+        if(node) {
+            r["channel"] = node->value();
+        }
+
+        // WiFi status
+        node = rootnode->first_node("wifi");
+        if(node) {
+            r["wifi"] = node->value();
+        }
+
+        // Bandwidth
+        node = rootnode->first_node("band_width");
+        if(node) { 
+            r["band_width"] = node->value();
+        }
+
+        // Security
+        node = rootnode->first_node("enc_type");
+        if(node) {
+            r["enc_type"] = node->value();
+        }
+
+        // TODO: User list
+
+    }
+
+    return r;
 }
 
-std::string JioFi::wan_info() {
+result_map JioFi::wan_info() {
     const std::string cmd_url = "st_wan.w.xml";
     const std::string url = deviceurl + cmd_url;
 
+    // fetch data in XML form
     std::string info_xml = fetch_data(url);
-    std::string info_json = xml2json(info_xml.c_str());
 
-    return info_json;
+    result_map r;
+
+    rapidxml::xml_document<> xml_data;
+    xml_data.parse<0>(&info_xml[0]);
+
+    rapidxml::xml_node<> *rootnode = xml_data.first_node();
+    if(rootnode) {
+        /*
+         * Extract fields
+         */
+        rapidxml::xml_node<> *node;
+
+        // Total Uplink
+        node = rootnode->first_node("t_tx");
+        if(node) {
+            r["t_tx"] = node->value();
+        }
+
+        // Total downlink
+        node = rootnode->first_node("t_rx");
+        if(node) {
+            r["t_rx"] = node->value();
+        }
+
+        // Packet loss per session
+        node = rootnode->first_node("plps");
+        if(node) {
+            r["plps"] = node->value();
+        }
+    }
+
+    return r;
 }
 
-std::string JioFi::dev_details() {
+result_map JioFi::dev_details() {
     const std::string cmd_url = "st_dev.w.xml";
     const std::string url = deviceurl + cmd_url;
 
+    // fetch data in XML form
     std::string info_xml = fetch_data(url);
-    std::string info_json = xml2json(info_xml.c_str());
 
-    return info_json;
+    result_map r;
+
+    rapidxml::xml_document<> xml_data;
+    xml_data.parse<0>(&info_xml[0]);
+
+    rapidxml::xml_node<> *rootnode = xml_data.first_node();
+    if(rootnode) {
+        /*
+         * Extract fields
+         */
+        rapidxml::xml_node<> *node;
+
+        // Battery status (dsc)
+        node = rootnode->first_node("dsc");
+        if(node) {
+            r["dsc"] = node->value();
+        }
+
+        // Battery percentage
+        node = rootnode->first_node("batt_per");
+        if(node) {
+            r["batt_per"] = node->value();
+        }
+
+        // Charging status
+        node = rootnode->first_node("batt_st");
+        if(node) {
+            r["batt_st"] = node->value();
+        }
+    }
+
+    return r;
 }
 
-std::string JioFi::sys_perf() {
+result_map JioFi::sys_perf() {
     const std::string cmd_url = "st_per.w.xml";
     const std::string url = deviceurl + cmd_url;
 
+    // fetch data in XML form
     std::string info_xml = fetch_data(url);
-    std::string info_json = xml2json(info_xml.c_str());
 
-    return info_json;
+    result_map r;
+
+    rapidxml::xml_document<> xml_data;
+    xml_data.parse<0>(&info_xml[0]);
+
+    rapidxml::xml_node<> *rootnode = xml_data.first_node();
+    if(rootnode) {
+        /*
+         * Extract fields
+         */
+        rapidxml::xml_node<> *node;
+        std::string raw_info;
+        std::vector<std::string> values;
+
+        // CPU utilization
+        node = rootnode->first_node("cpu");
+        if(node) {
+            raw_info = node->value();
+            values = parse_csv(raw_info);
+            r["cpu_usg_min"] = values[0];
+            r["cpu_usg_max"] = values[1];
+        }
+
+        // Memory usage
+        node = rootnode->first_node("mem");
+        if(node) {
+            raw_info = node->value();
+            values = parse_csv(raw_info);
+            r["mem_usg_min"] = values[0];
+            r["mem_usg_max"] = values[1];
+        }
+
+        // Current Uplink speed
+        node = rootnode->first_node("curr_tx");
+        if(node) {
+            r["curr_tx"] = node->value();
+        }
+
+        // Max Uplink speed in this session
+        node = rootnode->first_node("max_tx");
+        if(node) {
+            r["max_tx"] = node->value();
+        }
+
+        // min Uplink speed in this session
+        node = rootnode->first_node("min_tx");
+        if(node) {
+            r["min_tx"] = node->value();
+        }
+
+        // Current downlink speed
+        node = rootnode->first_node("curr_rx");
+        if(node) {
+            r["curr_rx"] = node->value();
+        }
+
+        // Max downlink speed
+        node = rootnode->first_node("max_rx");
+        if(node) {
+            r["max_rx"] = node->value();
+        }
+
+        // Min downlink speed
+        node = rootnode->first_node("min_rx");
+        if(node) {
+            r["min_rx"] = node->value();
+        }
+    }
+
+    return r;
 }
-
-std::string JioFi::generic_info() {
-    const std::string cmd_url = "mark_title.w.xml";
-    const std::string url = deviceurl + cmd_url;
-
-    std::string info_xml = fetch_data(url);
-    std::string info_json = xml2json(info_xml.c_str());
-
-    return info_json;
-}
-*/
-
-
-
-
